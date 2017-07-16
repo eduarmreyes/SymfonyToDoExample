49f508f41edf569a48fd1693b0a129f86c3048f4
eduarmreyes <eduar.mreyes@gmail.com>
2017-07-16 08:19:00 -0600

    Add Form for Tasks

---
+++ backend/src/Rootstack/TaskBundle/Form/TaskType.php
@@ -0,0 +1,45 @@
+<?php
+namespace Rootstack\TaskBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaskType extends AbstractType
+{
+/**
+ * @param FormBuilderInterface $builder
+ * @param array                $options
+ */
+public function buildForm(FormBuilderInterface $builder, array $options)
+{
+  $builder
+    ->add('title')
+    ->add('description')
+    ->add('isDone')
+  ;
+}
+
+/**
+ * @param OptionsResolver $resolver
+ */
+public function configureOptions(OptionsResolver $resolver)
+{
+
+  $resolver->setDefaults(
+    [
+	    'csrf_protection' => false,
+	    'data_class' => 'Rootstack\TaskBundle\Entity\Task',
+    ]
+  );
+}
+
+/**
+ * @return string
+ */
+public function getName()
+{
+  return "";
+}
+
+}
